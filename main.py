73049e644d4c80e981b959773e8c10010465bbbb
ppiszczekcode <p.piszczek.stuff+github@gmail.com>
2025-05-19 15:07:10 +0200

    'finished-for-now'

--- main.py
+++ main.py
@@ -2,39 +2,72 @@ from datetime import datetime
 import json
 import logging
 import os
+from time import sleep
 from typing import Any
 from src.spotdl_wrapper import SpotdlWrapper
+from src.spotdl_wrapper import colored_text
 
 def Main() -> None:
-
-    logger: logging.Logger = logging.getLogger("FileOrganizer")
-    if not os.path.exists("logs"):
-        os.mkdir("logs")
-
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        filename=os.path.join("logs", datetime.strftime(datetime.now(), format="%Y-%m-%d.log"))
-    )
-    logger.info("Logger initialized")
-
-    os.system('cls')
-    print("AUDIO DOWNLOADER", end="\n\n")
-
-    SETTINGS: dict[str, Any] = {}
     try:
-        with open(os.path.join("data", "settings.json")) as f:
-            SETTINGS = json.load(f)
-    except FileNotFoundError:
-        print("file not found, using default settings")
-    
-    spotdl_wrapper: SpotdlWrapper = SpotdlWrapper(settings=SETTINGS, logger=logger)
+        os.system('cls')
 
-    while True:
-        url: str = input('paste url\n')
-        # url: str =  "https://open.spotify.com/album/3fG8R9opVLvddvkUQ90feC?si=fb3a5afa5d6f422d"
+        logger: logging.Logger = logging.getLogger("FileOrganizer")
+        if not os.path.exists("logs"):
+            os.mkdir("logs")
 
-        spotdl_wrapper(url)
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+            filename=os.path.join("logs", datetime.strftime(datetime.now(), format="%Y-%m-%d.log"))
+        )
+        logger.info("Logger initialized")
+
+
+        SETTINGS: dict[str, Any] = {}
+        try:
+            with open(os.path.join("data", "settings.json")) as f:
+                SETTINGS = json.load(f)
+        except FileNotFoundError:
+            print
+            colored_text("Settings not found, using default settings", "red", "normal")
+
+
+        sleep(1)
+        os.system('cls')
+        ascii = r'''
+        
+   ____          __     ____  _      __                          
+  / __/__  ___  / /____/ / / | | /| / /______ ____  ___  ___ ____
+ _\ \/ _ \/ _ \/ __/ _  / /  | |/ |/ / __/ _ `/ _ \/ _ \/ -_) __/
+/___/ .__/\___/\__/\_,_/_/   |__/|__/_/  \_,_/ .__/ .__/\__/_/   
+   /_/                                      /_/  /_/             
+
+                        
+       '''
+
+        colored_text('Welcome to my cli playlist downloader', "green", "bright")
+        colored_text(ascii, "green", "dim")
+        
+        spotdl_wrapper: SpotdlWrapper = SpotdlWrapper(settings=SETTINGS, logger=logger)
+
+        while True:
+            url: str = input('Paste playlist url:\n> ').strip()
+            spotdl_wrapper(url)
+
+
+    except KeyboardInterrupt:
+            print("\nExiting...")
+
+
 
 if __name__ == '__main__':
-    Main()
+    while True:
+        Main()
+        try:
+            retry = input("Try again? Yes(y) / No(q):\n> ").strip().lower()
+            if retry not in ("y", "yes"):
+                print(" Exiting.")
+                break
+        except KeyboardInterrupt:
+            print("\nExiting...")
+            break
--- src/spotdl_wrapper.py
+++ src/spotdl_wrapper.py
@@ -1,3 +1,7 @@
+from colorama import Fore, Style, init
+import re
+import requests
+import yt_dlp
 from logging import Logger
 import os
 from typing import Any
@@ -16,7 +20,6 @@ class SpotdlWrapper:
 
         self.logger: Logger = logger
         self.logger.info("SpotdlWrapper initialized")
-                         
         self.ensureOutputPath()
 
     def __call__(self, url:str) -> None:
@@ -26,19 +29,67 @@ class SpotdlWrapper:
         Args:
             url (str): url to song 
         """       
+
+        def get_youtube_playlist_name(url) -> str | None:
+            '''
+            return youtube playlist name from url via api
+            '''
+            print('getting playlist name..')
+            try:
+                ydl_opts = {
+                    "quiet": True,  
+                    "extract_flat": True, 
+                    "playlist_items": "0",
+                    "socket_timeout": 3, 
+                    "force_generic_extractor": True,
+                    "ignoreerrors": True  
+                }
+                with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+                    if info := ydl.extract_info(url, download=False):
+                        print('success!')
+                        return info.get('title')
+            except Exception as e:
+                print(f"YouTube error: {type(e).__name__}")
+            return None
+
+
+        def get_spotify_playlist_name(url) -> str | None:
+            """
+            return spotify playlist name from url via api
+            """
+            print('getting playlist name..')
+            try:
+                response = requests.get(url, timeout=5)
+                response.raise_for_status()
+                title_match = re.search(r'<title>(.+?)</title>', response.text)
+                
+                print('success!')
+                return title_match.group(1).replace(" | Spotify", "").strip() if title_match else None
+            except Exception as e:
+                print(f"Error: {e}")
+                return None
+            
         platform: str = ""
+        playlist_name: str = ""
+        output: str = ''
 
         if url.find("youtube") != -1:
             platform = "youtube"
+            playlist_name = get_youtube_playlist_name(url)
+
         elif url.find("spotify") != -1:
             platform = "spotify"
+            playlist_name = get_spotify_playlist_name(url)
 
-        path = os.path.join(self.outputPath, platform)
+        path = os.path.join(self.outputPath, platform, playlist_name)
+        output = f'"{os.path.join(path, "{artist} - {title}.mp3")}"'
+        # print(output)
 
         skip_album_art = "--skip-album-art" if self.skipAlbumArt else ""
         print_errors = "--print-errors" if self.printErrors else ""
 
-        command = f"python -m spotdl download {url} --bitrate {self.bitrate} --format {self.format} {skip_album_art} {print_errors} --overwrite {self.overwrite} --threads {self.threads} --output {path}"
+        command = f"python -m spotdl download {url} --bitrate {self.bitrate} --format {self.format} --overwrite {self.overwrite} --threads {self.threads} --output {output} {skip_album_art} {print_errors}"
+        print(command)
 
         self.logger.debug(f"executing command: {command}")
         os.system(command)
@@ -57,6 +108,30 @@ class SpotdlWrapper:
             if not os.path.exists(os.path.join(self.outputPath, platform)):
                  self.logger.debug(f"{self.outputPath}/{platform} doesn't exists, creating")
                  os.mkdir(os.path.join(self.outputPath, platform))
+
+
+init(autoreset=True)
+def colored_text(text: str, color: str = 'white', style: str = 'normal') -> None:
+    colors = {
+        "black": Fore.BLACK,
+        "red": Fore.RED,
+        "green": Fore.GREEN,
+        "yellow": Fore.YELLOW,
+        "blue": Fore.BLUE,
+        "magenta": Fore.MAGENTA,
+        "cyan": Fore.CYAN,
+        "white": Fore.WHITE
+    }
+    styles = {
+        "dim": Style.DIM,
+        "normal": Style.NORMAL,
+        "bright": Style.BRIGHT
+    }
+    
+    color_code = colors.get(color.lower(), Fore.WHITE)
+    style_code = styles.get(style.lower(), Style.NORMAL)
+
+    print(style_code + color_code + text + Style.RESET_ALL)
 
 
 if __name__ == "__main__":
